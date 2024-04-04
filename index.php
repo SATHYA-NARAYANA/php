8fd69026b1d7bf404531b54fb5b336565418b0c4
Sathya Narayana <54190045+SATHYA-NARAYANA@users.noreply.github.com>
2024-04-05 00:15:37 +0530

    Create index.php
    Test PHP code to text httpd webserver

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Welcome to Sathya Web Server for Testing</title>
+</head>
+<body style="background-color: aqua;">
+    <pre>
+        <?php
+            print "Welcome to Sathya Web Server for Testing\n";
+            print `ifconfig`;
+        ?>
+    </pre>
+</body>
+</html>
